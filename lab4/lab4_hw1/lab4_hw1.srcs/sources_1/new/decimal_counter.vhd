50abbd434c559ae2c8bca3b1b63d153b068b6964
Wendy-Ying <12210159@mail.sustech.edu.cn>
2025-03-18 13:24:15 +0800

    update hw3 and include hw4

---
+++ lab4/lab4_hw1/lab4_hw1.srcs/sources_1/new/decimal_counter.vhd
@@ -0,0 +1,82 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 2025/03/17 16:52:23
+-- Design Name: 
+-- Module Name: decimal_counter - concurrent_arcg
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.std_logic_arith.ALL;
+use IEEE.std_logic_unsigned.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity decimal_counter is
+    Port ( CLK : in STD_LOGIC;
+           RESET : in STD_LOGIC;
+           d1 : out std_logic_vector (3 downto 0);
+           d10 : out std_logic_vector (3 downto 0);
+           d100 : out std_logic_vector (3 downto 0));
+end decimal_counter;
+
+architecture concurrent_arch of decimal_counter is
+
+signal d1_reg, d10_reg, d100_reg: std_logic_vector (3 downto 0);
+signal d1_next, d10_next, d100_next: std_logic_vector (3 downto 0);
+
+signal clock_divider : std_logic_vector (26 downto 0) := (others => '0');
+
+begin
+
+-- register
+process (CLK, RESET) is
+begin
+    if RESET = '1' then
+        d1_reg <= "0000";
+        d10_reg <= "0000";
+        d100_reg <= "0000";
+    elsif CLK'event and CLK='1' then
+        if clock_divider = "101111101011110000011111111" then
+            clock_divider <= (others => '0');
+            d1_reg <= d1_next;
+            d1_reg <= d10_next;
+            d1_reg <= d100_next;
+        else
+            clock_divider <= clock_divider + 1;
+        end if;
+    end if;
+end process;
+
+-- next_state logic
+d1_next <= "0000" when d1_reg = 9 else d1_reg+1;
+d10_next <= "0000" when (d1_reg = 9 and d10_reg = 9) else d10_reg+1 when d1_reg = 9 else d10_reg;
+d100_next <= "0000" when (d1_reg = 9 and d10_reg = 9 and  d100_reg = 9) else d100_reg+1 when (d1_reg = 9 and d10_reg = 9)  else d100_reg;
+
+-- output logic
+d1 <= d1_reg;
+d10 <= d10_reg;
+d100 <= d100_reg;
+
+end concurrent_arch;
